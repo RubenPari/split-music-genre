ec2eae9e9e6d9ee93875ab7a8e45d4f5ca35db7a
RubenPari <rubenpari.developer@gmail.com>
2024-08-16 17:24:32 +0200

    initial commit

---
+++ src/main/java/io/rubenpari/splitmusicgenre/SplitMusicGenreApplication.java
@@ -0,0 +1,13 @@
+package io.rubenpari.splitmusicgenre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitMusicGenreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitMusicGenreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/rubenpari/splitmusicgenre/SplitMusicGenreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rubenpari.splitmusicgenre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitMusicGenreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
